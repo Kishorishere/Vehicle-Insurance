e9ecab0962fdd602ca2dbd4733d1b96fed9495d0
Kishor Timilsena <kishortimilsena2@gmail.com>
2025-03-20 15:08:31 +0545

    project structure setup along with venv

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Kishor Timilsena",
+    author_email="kh0750234@gmail.com",
+    packages=find_packages()
+)
